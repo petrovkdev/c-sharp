80966179676a5c63cd13067b055b510cb9e10abb
petrovk <petrovk86@mail.ru>
2019-08-27 00:46:25 +0400

    solution Izh_04_Basic_Coding

---
+++ Izh_04_Basic_Coding/Izh_04_Basic_Coding/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Izh_04_Basic_Coding")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("Izh_04_Basic_Coding")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dcfd825f-2b29-4f05-b51d-a577556ea2c0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Izh_04_Basic_Coding/Solution/BasicCoding.cs
@@ -0,0 +1,143 @@
+﻿// <copyright file="BasicCoding.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    public static class BasicCoding
+    {
+        public static int InsertNumber(int numberSource, int numberLn, int i, int j)
+        {
+            if (i == j)
+            {
+                if (numberSource == numberLn)
+                {
+                    numberLn &= numberSource;
+                }
+                else
+                {
+                    numberLn = (numberLn & numberSource) + 1;
+                }
+            }
+
+            return numberLn << i;
+        }
+
+        public static int CheckFindingElementIndexBetweenEqualSums(int[] sourceArray)
+        {
+            int max = 0;
+
+            for (int i = 0; i < sourceArray.Length; i++)
+                if (max < sourceArray[i]) max = sourceArray[i];
+
+            return max;
+        }
+
+        public static int CheckFindingElementIndexBetweenEqualSums(double[] sourceArray)
+        {
+            int index = -1;
+
+            for (int i = 0; i < sourceArray.Length; i++)
+            {
+                double a = 0, b = 0;
+
+                for (int z = 0; z < sourceArray.Length; z++)
+                {
+                    if (z < i) a += sourceArray[z];
+
+                    if (z > i) b += sourceArray[z];
+                }
+
+                if (a.ToString() == b.ToString()) index = i;
+            }
+
+            return index;
+        }
+
+        public static string CheckStringConcatenation(string firstStr, string secondStr)
+        {
+            string strSecond = null;
+
+            for (int f = 0; f < secondStr.Length; f++)
+            {
+                if (firstStr.IndexOf(secondStr[f]) != -1) continue;
+                strSecond += secondStr[f];
+            }
+
+            return firstStr + strSecond;
+        }
+
+        public static int FindNextBiggerNumber(int number)
+        {
+            Stopwatch time = new Stopwatch();
+
+            time.Start();
+
+            string s = "Число - " + number + ". Время нахождения составило - ";
+
+            char[] numbers = number.ToString().ToCharArray();
+
+            for (int i = numbers.Length - 1; i > 0; i--)
+            {
+                int value = numbers[i];
+                int prevValue = numbers[i - 1];
+
+                if (value > prevValue)
+                {
+                    numbers[i] = (char)prevValue;
+                    numbers[i - 1] = (char)value;
+
+                    int count = numbers.Length - i;
+
+                    char[] newNumbers = new char[count];
+
+                    Array.ConstrainedCopy(numbers, i, newNumbers, 0, count);
+
+                    Array.Sort(newNumbers);
+
+                    Array.Resize(ref numbers, i);
+
+                    time.Stop();
+
+                    Console.WriteLine(s + time.Elapsed);
+
+                    return int.Parse(new string(numbers) + new string(newNumbers));
+                }
+            }
+
+            time.Stop();
+
+            Console.WriteLine(s + time.Elapsed);
+
+            return -1;
+        }
+
+        public static int[] FilterDigit(int[] sourceArray, int digit)
+        {
+            int[] distArray = new int[] { };
+
+            for (int i = 0; i < sourceArray.Length; i++)
+            {
+                if (sourceArray[i].ToString().IndexOf(digit.ToString()) == -1) continue;
+                Array.Resize(ref distArray, distArray.Length + 1);
+                distArray[distArray.Length - 1] = sourceArray[i];
+            }
+
+            return distArray;
+        }
+
+        private static void Main(string[] args)
+        {
+            int n = InsertNumber(8, 15, 0, 0);
+
+            Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
